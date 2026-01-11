07fc10604146901692959add84604a1a117d6fda
Christian Klein <xcklein@gmail.com>
2026-01-10 19:42:00 -0600

    add tags

--- infra/app.ts
+++ infra/app.ts
@@ -1,4 +1,4 @@
-import { App, Aspects } from "aws-cdk-lib/core";
+import { App, Aspects, Tags } from "aws-cdk-lib/core";
 import { AwsSolutionsChecks } from "cdk-nag";
 import { GithubStack } from "./github-stack";
 
@@ -8,6 +8,9 @@ const user = app.node.getContext("user");
 const repo = app.node.getContext("repo");
 
 new GithubStack(app, "GithubStack", { user, repo });
+
+Tags.of(app).add("x:project", "my-project");
+Tags.of(app).add("x:team", "my-team");
 
 Aspects.of(app).add(new AwsSolutionsChecks());
 
